19ae6d51a08837e4d45bf207fa250fc255060429
toptsundere <30511480+toptsundere@users.noreply.github.com>
2018-04-15 21:58:48 +0200

    Layout & initial audio playing

--- app/src/main/java/kawaiitsundere/soundboard/MainActivity.java
+++ app/src/main/java/kawaiitsundere/soundboard/MainActivity.java
@@ -1,7 +1,16 @@
 package kawaiitsundere.soundboard;
 
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Typeface;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Action bar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        //Custom title font
+        TextView titleText = (TextView) findViewById(R.id.titleText);
+        Typeface ralewayFont = Typeface.createFromAsset(getAssets(), "fonts/raleway_medium.ttf");
+        titleText.setTypeface(ralewayFont);
+
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void playAudioTest(View view){
+        MediaPlayer mp = new MediaPlayer();
+
+        try {
+            AssetFileDescriptor descriptor = getAssets().openFd("testaudio.mp3");
+
+            mp.setDataSource(descriptor.getFileDescriptor(), descriptor.getStartOffset(), descriptor.getLength());
+            descriptor.close();
+
+            mp.prepare();
+            mp.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
